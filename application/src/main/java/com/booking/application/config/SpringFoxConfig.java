71e302d4dfe3f43ce34cd35dabc803020b525d9e
Victor Olamide <akinnusivictor098@gmail.com>
2023-07-17 12:23:41 +0100

    App user registration

--- application/src/main/java/com/booking/application/config/SpringFoxConfig.java
+++ application/src/main/java/com/booking/application/config/SpringFoxConfig.java
@@ -9,10 +9,15 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.List;
 import java.util.Objects;
 
+
+@Configuration
+@EnableWebMvc
 public class SpringFoxConfig {
 
     @Value("${spring.datasource.url}")
